9f1561c60138fd9adf13a50eb691e85b0c7679c3
Ramdhanll <bscdhani30@gmail.com>
2020-02-21 16:13:04 +0700

    After create Frontend

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header></Header>
+    <Hero></Hero>
+    <Woman></Woman>
+    <Instagram></Instagram>
+    <PartnerLogo></PartnerLogo>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '../components/Header.vue'
+import Hero from '../components/Hero.vue'
+import Woman from '../components/Woman.vue'
+import Instagram from '../components/Instagram.vue'
+import PartnerLogo from '../components/PartnerLogo.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    Woman,
+    Instagram,
+    PartnerLogo,
+    Footer
+  }
+}
+</script>
